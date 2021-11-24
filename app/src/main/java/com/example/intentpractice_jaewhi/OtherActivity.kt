4aa03f12283ca995db4e72c0cd9751b8087ac4de
Tycoon19 <kevin960831@naver.com>
2021-11-24 21:10:57 +0900

    finish 이용 -> 현재화면을 종료해서 이전화면으로 복귀하기

--- app/src/main/java/com/example/intentpractice_jaewhi/OtherActivity.kt
+++ app/src/main/java/com/example/intentpractice_jaewhi/OtherActivity.kt
@@ -1,11 +1,25 @@
 package com.example.intentpractice_jaewhi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+//        이전 화면으로 돌아가기 (MainActivity)
+
+        returnToMainBtn.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+            finish()
+
+        }
+
     }
 }
