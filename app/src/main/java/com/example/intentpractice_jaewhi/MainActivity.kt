ce49708e99647cb4c83f119aff0474f6c84df80b
Tycoon19 <kevin960831@naver.com>
2021-11-26 22:59:41 +0900

    CALL 액션 예제 구현 - 권한을 얻지 못해서 에러 발생.(앱이 죽음)

--- app/src/main/java/com/example/intentpractice_jaewhi/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice_jaewhi/MainActivity.kt
@@ -14,6 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        CALL 액션 예제
+
+        callBtn.setOnClickListener {
+
+            val inputPhoneNum = phoneNumEdt.text.toString()
+
+            val myUrl = Uri.parse("tel:${inputPhoneNum}")
+
+            val myIntent = Intent(Intent.ACTION_CALL,myUrl)
+
+            startActivity(myIntent)
+
+        }
+
 //        DIAL 액션 예제
 
         dialBtn.setOnClickListener {
